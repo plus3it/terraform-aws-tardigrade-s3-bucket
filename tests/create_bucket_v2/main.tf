226890dc1262aec39d50447c1f07b67fcacb92e0
Long C Lam <31355535+eemperor@users.noreply.github.com>
2020-08-07 20:43:55 +0000

    Adds test for v2.7 of AWS provider

---
+++ tests/create_bucket_v2/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  version = "~> 2.7"
+  region  = "us-east-1"
+}
+
+resource "random_id" "name" {
+  byte_length = 6
+  prefix      = "tardigrade-s3-bucket-"
+}
+
+module "create_bucket_v2" {
+  source = "../../"
+  providers = {
+    aws = aws
+  }
+
+  create_bucket = true
+  bucket        = random_id.name.hex
+  tags = {
+    environment = "testing"
+  }
+}
+
+output "create_bucket_v2" {
+  value = module.create_bucket_v2
+}
