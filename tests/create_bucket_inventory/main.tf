268a8a8012fe33b62f12f3b43dfd0bc728f119ca
cahnk <99229283*cahnk@users.noreply.github.com>
2022-04-11 12:38:42 -0600

    Linting issues

--- tests/create_bucket_inventory/main.tf
+++ tests/create_bucket_inventory/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "inventory" {
   bucket = format("%s-%s", "inventory", random_id.name.hex)
 }
 
-/*moto has not implemented the bucket_inventory_configuration yet, so the CI test fails while running pytest.  The test passes when run against an actual AWS account.  We need to uncomment the folloiwng module when moto catches up.  
+/*moto has not implemented the bucket_inventory_configuration yet, so the CI test fails while running pytest.  The test passes when run against an actual AWS account.  We need to uncomment the folloiwng module when moto catches up.
 module "create_bucket" {
   source = "../../"
 
@@ -33,5 +33,5 @@ module "create_bucket" {
         prefix     = null
       }
     }
-  } 
+  }
 } */
