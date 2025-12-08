b52a9027157d73e1aceed0c55a1eec82f2afc542
Philip Sciortino <philipsciortino@Philips-MacBook-Pro.local>
2025-12-08 11:07:30 -0500

    Fix SSE count evaluation

--- main.tf
+++ main.tf
@@ -296,7 +296,7 @@ resource "aws_s3_bucket_versioning" "this" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  count = var.server_side_encryption_configuration == null ? 0 : 1
+  count = length(var.server_side_encryption_configuration) == 0 ? 0 : 1
 
   bucket = aws_s3_bucket.this.id
 
